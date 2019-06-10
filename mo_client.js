2d89b49d204eed48e499642a05cec4285efff6c3
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-06-10 15:01:32 +0900

    wip..

--- mo_client.js
+++ mo_client.js
@@ -26,6 +26,15 @@ g_ws.onmessage = function (ev) {
 
 
 ///////////
+var g_gridcursor;
+function setupGridCursor() {
+    g_gridcursor = new Prop2D();
+    g_gridcursor.setDeck(g_base_deck);
+    g_gridcursor.setScl(20,20);
+    g_gridcursor.setIndex(193);
+    g_main_layer.insertProp(g_gridcursor);
+}
+
 
 function gameInit() {
 
@@ -52,7 +61,11 @@ function gameInit() {
     
     setupFieldGrid();
 
+    setupGridCursor();
+    
     setGameState("stopped");
+
+    
 }
 
 var g_game_state;
@@ -61,37 +74,84 @@ function setGameState(state) {
     var e=document.getElementById("state");
     e.innerHTML = "Game " + state;
 }
-
-
+var g_game_role=undefined;
+// role: "host" or "guest"
+function setGameRole(role) {
+    var color = (role=="host" ? "red" : "blue");
+    var e=document.getElementById("role");
+    e.innerHTML="Role: " + role + `(<span style='color:${color};'>${color}</span>)`;
+    g_game_role=role;
+}
+function updateGameTurn() {
+    var e=document.getElementById("turn").innerHTML="Turn: "+g_turn;
+}
+function updateSoldierLeft() {
+    var col = (g_game_role=="host" ? "red":"blue");
+    document.getElementById(col+"left").innerHTML=`${g_soldier_left} left`;
+}
+var g_soldier_left=0;
+function gameUpdate() {
+    g_turn++;
+    updateGameTurn();
+    if(g_turn%120==0) {
+        g_soldier_left++;
+        if(g_soldier_left>10) g_soldier_left=10;
+        updateSoldierLeft();
+    }
+    
+}
+//////////////
+function clickOnField(gx,gy) {
+    console.log("clickOnField:",gx,gy);
+}
 //////////////
 
 
 var anim_cnt=0;
 var last_anim_at = new Date().getTime();
-
+var g_turn=0;
 
 function animate() {
     anim_cnt++;
 	requestAnimationFrame( animate );
 
-    if(g_mouse.getButton(0)) {
-        var x=g_mouse.cursor_pos[0]-SCRW/2;
-        var y=SCRH/2-g_mouse.cursor_pos[1];
+    var x=g_mouse.cursor_pos[0]-SCRW/2;
+    var y=SCRH/2-g_mouse.cursor_pos[1];
+    var gx=Math.floor((x+SCRW/2)/20), gy=Math.floor((y+SCRH/2)/20);    
+    if(g_mouse.getToggled(0)) {
+        g_mouse.clearToggled(0);
         console.log("mousebutton0:",x,y);
+        clickOnField(gx,gy);
     }
     if(g_touch.touching) {
         var x=g_touch.last_touch_pos[0]-SCRW/2;
         var y=SCRH/2-g_touch.last_touch_pos[1];
         console.log("touchat:",x,y);
     }
+    if(g_gridcursor) {
+        g_gridcursor.setLoc(gx*20-SCRW/2+10,gy*20-SCRH/2+10);
+        var cant=false;
+        if(g_game_state!="started") {
+            cant=true;
+        } else {
+            if(g_game_role=="host" && gx>14){
+                cant=true;
+            } else if(g_game_role=="guest" && gx<17) {
+                cant=true
+            }
+        }
+        g_gridcursor.setIndex(cant?194:193);
+    }
+
+    //////
     var now_time = new Date().getTime();
     var dt = now_time - last_anim_at;
     last_anim_at = now_time;
     Moyai.poll(dt/1000.0);
     Moyai.render();
 
-    if(g_game_state=="started") {
-        
+    if(g_game_state=="started" ) {
+        gameUpdate();
     }
 }
 
@@ -104,11 +164,13 @@ gameInit();
 ////////////
 function createRoomPressed() {
     send_createRoom(g_ws);
+    setGameRole("host");    
 }
 function joinRoomPressed() {
     var roomid=parseInt(document.getElementById("roomid").value);
     send_joinRoom(g_ws,roomid);
     setGameState("waiting");
+    setGameRole("guest");
 }
 
 
@@ -127,8 +189,10 @@ recv_joinNotify = function(conn) {
     appendLog("recv_joinNotify");
     send_gameStart(conn);
     setGameState("started");
+    g_turn=0;
 }
 recv_gameStart = function(conn) {
     appendLog("recv_gameStart");
     setGameState("started");
+    g_turn=0;    
 }
